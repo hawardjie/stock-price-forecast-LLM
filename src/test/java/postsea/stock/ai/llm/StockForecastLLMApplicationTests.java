ac31b6dfd70b8f3a4e5efc58188bcf07ec1f3b17
Haward Jie <haward.jie@gmail.com>
2024-12-09 22:44:14 -0500

    Add Test file

---
+++ src/test/java/postsea/stock/ai/llm/StockForecastLLMApplicationTests.java
@@ -0,0 +1,13 @@
+package postsea.stock.ai.llm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockForecastLLMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
